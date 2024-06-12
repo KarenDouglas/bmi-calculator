966fe2ffab3d26d928804719f49394bff33096fa
Karen Douglas <kdindependence@gmail.com>
2024-06-12 16:19:40 -0500

    set up BMI Calc Component base

---
+++ client/src/components/BmiCalcComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BmiCalcComponent() {
+
+    const place = 'World'
+  
+    return (
+      <>
+        <h1>Hello {place}</h1>
+      </>
+    )
+  }
+  
+  export default BmiCalcComponent
