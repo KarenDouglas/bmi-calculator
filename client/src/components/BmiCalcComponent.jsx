5085d136243359529a25df14f61820c543f8745e
Karen Douglas <kdindependence@gmail.com>
2024-06-17 16:43:06 -0500

    failed test due to jest not interpreting images

--- client/src/components/BmiCalcComponent.jsx
+++ client/src/components/BmiCalcComponent.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import logo from "../assets/logo.svg"
 
 function BmiCalcComponent() {
 
-    const place = 'World'
   
     return (
       <>
-        <h1>Hello World</h1>
+       <main>
+        <section>
+            <img src={logo} alt="BMI Calculator Logo" />
+            <figure aria-label="background image">
+                <h1>Body Mass Index Calculator</h1>
+                <p aria-label="Introduction to BMI Calculator">Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
+            </figure>
+        </section>
+       </main>
       </>
     )
   }
