62aa25e96d03cc425821a59bec6e57501cbbc54e
Karen Douglas <kdindependence@gmail.com>
2024-06-18 14:47:50 -0500

    got second test / form test in BMI component test to pass

--- client/src/components/BmiCalcComponent.jsx
+++ client/src/components/BmiCalcComponent.jsx
@@ -10,10 +10,28 @@ function BmiCalcComponent() {
         <section>
             <img src={logo} alt="BMI Calculator Logo" />
             <figure aria-label="background image">
-                <h1>Body Mass Index Calculator</h1>
-                <p aria-label="Introduction to BMI Calculator">Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
             </figure>
         </section>
+        <section>
+                <h1>Body Mass Index Calculator</h1>
+                <p aria-label="Introduction to BMI Calculator">Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
+            </section>
+            <form data-testid="form">
+                <h3 aria-label="Enter your details below">Enter your details below</h3>
+                <label htmlFor="metric" >Metric</label>
+                <input type="radio" id="metric" checked/>
+                <label htmlFor="imperial">Imperial</label>
+                <input type="radio" id="imperial" aria-labelledby="imperial" />
+                <label htmlFor="height">Height</label>
+                <input type="number" id="height" />
+                <label htmlFor="weight">Weight</label>
+                <input type="number" id="weight" />
+            </form>
+            <section>
+                <h3 aria-label="Results header">Welcome!</h3>
+                <p aria-label="Results description">Enter our height and weight nd you'll see your BMI result here</p>
+
+            </section>
        </main>
       </>
     )
--- client/tests/bmiCalcComp.test.js
+++ client/tests/bmiCalcComp.test.js
@@ -3,14 +3,16 @@ import { render, screen } from '@testing-library/react';
 import BmiCalcComponent from '../src/components/BmiCalcComponent';
 
 
-// renders BMI Component
-render(<BmiCalcComponent />);
 
 
 
 describe('test that all visible aspects of the BMI Component are rendered after loaded', () => {
 
+
     test('renders main heading and paragraph content', () => {
+            // renders BMI Component
+    render(<BmiCalcComponent />);
+
         // Elements to be tested
          const heading = screen.getByRole('heading', {name: 'Body Mass Index Calculator'});
          const paragraph = screen.getByRole('paragraph',{ name: "Introduction to BMI Calculator"})
@@ -20,12 +22,14 @@ describe('test that all visible aspects of the BMI Component are rendered after 
          expect(heading.innerHTML).toMatch('Body Mass Index Calculator');
          expect(paragraph).toBeDefined();
          expect(paragraph.innerHTML).toMatch('Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.');
-     });
-    
-     test('renders content of form in its default state', () => {
+        });
+
+    test('renders form content and inputs', () => {
         // elements to be tested
-       const form = screen.getByRole('form',{name:'BMI Calculator Form'});
-       const heading = screen.getByRole('heading', {name: "Enter your details below"})
+        // renders BMI Component
+        render(<BmiCalcComponent />);
+        const form = screen.getByTestId('form')
+        const formHeading = screen.getByRole('heading', {name: "Enter your details below"})
        const metricRadioOption = screen.getByRole('radio',{name: 'Metric'});
        const imperialOption = screen.getByRole('radio', {name: 'Imperial'});
        const heightInput = screen.getByLabelText('Height');
@@ -35,20 +39,18 @@ describe('test that all visible aspects of the BMI Component are rendered after 
        
        // testing all element in the form and content is rendered
        expect(form).toBeDefined();
-       expect(heading).toBeDefined();
-       expect(heading.innerHTML).toMatch('Enter your details below');
+       expect(formHeading).toBeDefined();
+       expect(formHeading.innerHTML).toMatch('Enter your details below');
        expect(metricRadioOption).toBeDefined();
        expect(metricRadioOption.checked).toBeTruthy();
        expect(imperialOption).toBeDefined();
        expect(imperialOption.checked).toBeFalsy();
        expect(heightInput).toBeDefined();
-       expect(heightInput.value).toBe(0)
        expect(weightInput).toBeDefined();
-       expect(weightInput.value).toBe(0);
        expect(resultsHeading).toBeDefined();
        expect(resultsHeading.innerHTML).toMatch('Welcome!');
        expect(resultParagraph).toBeDefined();
-       expect(resultParagraph).toMatch(`Enter our height and weight nd you'll see your BMI result here`);     
+       expect(resultParagraph.innerHTML).toMatch(`Enter our height and weight nd you'll see your BMI result here`);     
     });
     
     
