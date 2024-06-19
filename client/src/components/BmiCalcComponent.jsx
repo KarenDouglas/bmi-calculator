579ba608912593e809e6fd120831f267211a1c8e
Karen Douglas <kdindependence@gmail.com>
2024-06-19 16:59:09 -0500

    created test for bmi calc metric logic

--- client/src/components/BmiCalcComponent.jsx
+++ client/src/components/BmiCalcComponent.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../assets/logo.svg"
 
 function BmiCalcComponent() {
-
+    const [resultsHeaderAriaLabel, setResultsHeaderAriaLabel] = useState('Welcome header')
   
     return (
       <>
@@ -14,22 +14,26 @@ function BmiCalcComponent() {
         </section>
         <section>
                 <h1>Body Mass Index Calculator</h1>
-                <p aria-label="Introduction to BMI Calculator">Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
+                <p>Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
             </section>
-            <form data-testid="form">
+            <form data-testid="measurment-form">
                 <h3 aria-label="Enter your details below">Enter your details below</h3>
                 <label htmlFor="metric" >Metric</label>
                 <input type="radio" id="metric" checked/>
                 <label htmlFor="imperial">Imperial</label>
+                <form>
                 <input type="radio" id="imperial" aria-labelledby="imperial" />
                 <label htmlFor="height">Height</label>
-                <input type="number" id="height" />
+                <input type="text" id="height" />
                 <label htmlFor="weight">Weight</label>
-                <input type="number" id="weight" />
+                <input type="text" id="weight" />
+
+                </form>
             </form>
             <section>
                 <h3 aria-label="Results header">Welcome!</h3>
-                <p aria-label="Results description">Enter our height and weight nd you'll see your BMI result here</p>
+                <p  data-testid="results">25.8</p>
+                <p data-testid="results-description">Enter our height and weight nd you'll see your BMI result here</p>
 
             </section>
        </main>
--- client/tests/bmiCalcComp.test.js
+++ client/tests/bmiCalcComp.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import BmiCalcComponent from '../src/components/BmiCalcComponent';
 
 
@@ -10,48 +10,101 @@ describe('test that all visible aspects of the BMI Component are rendered after 
 
 
     test('renders main heading and paragraph content', () => {
-            // renders BMI Component
-    render(<BmiCalcComponent />);
+        // renders BMI Component
+        render(<BmiCalcComponent />);
 
         // Elements to be tested
          const heading = screen.getByRole('heading', {name: 'Body Mass Index Calculator'});
-         const paragraph = screen.getByRole('paragraph',{ name: "Introduction to BMI Calculator"})
+         const paragraph = screen.getByText('Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.')
      
          // testing that elements and correct content is rendered
          expect(heading).toBeDefined();
          expect(heading.innerHTML).toMatch('Body Mass Index Calculator');
          expect(paragraph).toBeDefined();
-         expect(paragraph.innerHTML).toMatch('Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.');
-        });
+    });
 
     test('renders form content and inputs', () => {
         // elements to be tested
         // renders BMI Component
         render(<BmiCalcComponent />);
-        const form = screen.getByTestId('form')
+        const form = screen.getByTestId('measurment-form')
         const formHeading = screen.getByRole('heading', {name: "Enter your details below"})
-       const metricRadioOption = screen.getByRole('radio',{name: 'Metric'});
-       const imperialOption = screen.getByRole('radio', {name: 'Imperial'});
-       const heightInput = screen.getByLabelText('Height');
-       const weightInput = screen.getByLabelText('Weight');
-       const resultsHeading = screen.getByRole('heading', {name: 'Results header'});
-       const resultParagraph = screen.getByRole('paragraph', {name: 'Results description'});
-       
-       // testing all element in the form and content is rendered
-       expect(form).toBeDefined();
-       expect(formHeading).toBeDefined();
-       expect(formHeading.innerHTML).toMatch('Enter your details below');
-       expect(metricRadioOption).toBeDefined();
-       expect(metricRadioOption.checked).toBeTruthy();
-       expect(imperialOption).toBeDefined();
-       expect(imperialOption.checked).toBeFalsy();
-       expect(heightInput).toBeDefined();
-       expect(weightInput).toBeDefined();
-       expect(resultsHeading).toBeDefined();
-       expect(resultsHeading.innerHTML).toMatch('Welcome!');
-       expect(resultParagraph).toBeDefined();
-       expect(resultParagraph.innerHTML).toMatch(`Enter our height and weight nd you'll see your BMI result here`);     
+        const metricRadioOption = screen.getByRole('radio',{name: 'Metric'});
+        const imperialOption = screen.getByRole('radio', {name: 'Imperial'});
+        const heightInput = screen.getByLabelText('Height');
+        const weightInput = screen.getByLabelText('Weight');
+        const resultsHeading = screen.getByRole('heading', {name: 'Results header'});
+        const resultParagraph = screen.getByText(`Enter our height and weight nd you'll see your BMI result here`);
+        
+        // testing all element in the form and content is rendered
+        expect(form).toBeDefined();
+        expect(formHeading).toBeDefined();
+        expect(formHeading.innerHTML).toMatch('Enter your details below');
+        expect(metricRadioOption).toBeDefined();
+        expect(metricRadioOption.checked).toBeTruthy();
+        expect(imperialOption).toBeDefined();
+        expect(imperialOption.checked).toBeFalsy();
+        expect(heightInput).toBeDefined();
+        expect(weightInput).toBeDefined();
+        expect(resultsHeading).toBeDefined();
+        expect(resultsHeading.innerHTML).toMatch('Welcome!');
+        expect(resultParagraph).toBeDefined();
+        expect(resultParagraph.innerHTML).toMatch(`Enter our height and weight nd you'll see your BMI result here`);     
     });
-    
+    test('should return BMI calculation using metric system', ()=> {
+        //-------- BMI CALCULATIONS NOTES------------
+        
+        //-----  HEALTHY BMI RANGE IS 18.5 kg/m² to 24.9 kg/m² -------
+        
+        //----- BMI CALCULATIONS------
+        // Height in meters (m): 165 cm / 100 cm/m = 1.65 m
+        // BMI Calculation:
+        // BMI = weight (kg) / (height (m))^2
+        // BMI = 50 kg / (1.65 m)^2 = 18.4 (rounded to two decimal places)
+        //-----END BMI CALCULATIONS END------
+        
+        //------ GETTING MAX & MIN HEALTHY WEIGHT RANGE ----
+        // Maximum Healthy Weight (kg) = BMI_max * height²        
+        // Maximum Healthy Weight (kg) = 24.9 kg/m² * (1.7 m)² ≈ 77.3 kg
+        // Minimum Healthy Weight (kg) = BMI_min * height²
+        // Minimum Healthy Weight (kg) = 18.5 kg/m² * (1.7 m)² ≈ 58.1 kg
+        //-----END GETTING MAX & MIN HEALTHY WEIGHT RANGE END------
+        
+        //--------END BMI CALCULATIONS NOTES END------------
+        // results decription should show more info
+        // test if classification is correct
+        
+        // renders BMI Component
+        render(<BmiCalcComponent />);
+        // Elements
+        const heightInput = screen.getByLabelText('Height');
+        const weightInput = screen.getByLabelText('Weight');
+        const resultsHeading = screen.getByRole('heading', {name: 'Results header'});
+        const resultsEl = screen.getByTestId('results');
+        const resultParagraph = screen.getByTestId('results-description');
+        const weightClassSpan = screen.getByTestId('weight-classification');
+        const minWeightSpan = screen.getByTestId('min-weight-range');
+        const maxWeightSpan = screen.getByTestId('max-weight-range');
+        const HEALTHY_WEIGHT = "a healthy weight";
+        const UNHEALTHY_WEIGHT = "an unhealty weight";
+        
+        // submits height and weight inputs
+        fireEvent.change(heightInput, {target: {value: 170}});
+        fireEvent.change(weightInput, {target: {value: 75}});
+
+        
+        expect(heightInput.value).toBe('170');
+        expect(weightInput.value).toBe('75');
+        expect(parseFloat(resultsEl.innerHTML)).toBeCloseTo(25.8);
+        expect(resultsHeading.innerHTML).toMatch('Your BMI is...');
+        expect(resultParagraph.innerHTML).toMatch(`Your BMI suggests you're at an unhealthy weight. Your ideal weight is between 53.5kgs-72kgs`);
+        expect(weightClassSpan.innerHTML).toMatch(UNHEALTHY_WEIGHT);
+        expect(parseFloat(minWeightSpan.innerHTML)).toBe(53.5)
+        expect(parseFloat(maxWeightSpan.innerHTML)).toBe(72)
+        
+
+    });
+
+
     
 });
