b7cd1f35600942156b689e320072b4090e95ab18
Karen Douglas <kdindependence@gmail.com>
2024-07-02 09:55:57 -0500

    styles advice component for small screens

--- client/src/components/AdviceComponent.jsx
+++ client/src/components/AdviceComponent.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import iconEating from '../assets/icon-eating.svg';
 import iconExercise from "../assets/icon-exercise.svg";
 import iconSleep from "../assets/icon-sleep.svg";
+import "../assets/css/adviceStyles.css";
 
 function AdviceComponent (){
 return(
     <>
-    <article>
+    <article className='advice-section'>
 
         <section>
             <i>
                 <img src={iconEating} alt="icon of ramen bowl" />
             </i>
             <section>
-                <h2>Healthy eating</h2>
+                <h3>Healthy eating</h3>
                 <p>
                     Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity, and mood.
                 </p>
@@ -24,7 +25,7 @@ return(
                 <img src={iconExercise} alt="icon of ramen bowl" />
             </i>
             <section>
-                <h2>Regular exercise</h2>
+                <h3>Regular exercise</h3>
                 <p>
                 Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity.
                 </p>
@@ -35,7 +36,7 @@ return(
                 <img src={iconSleep} alt="icon of ramen bowl" />
             </i>
             <section>
-                <h2>Adequate sleep</h2>
+                <h3>Adequate sleep</h3>
                 <p>
                 Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation.
                 </p>
