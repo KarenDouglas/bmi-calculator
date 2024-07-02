13a5b27d8969e4ed0f6c77453a38b21cde6f88e1
Karen Douglas <kdindependence@gmail.com>
2024-07-02 10:18:38 -0500

    styled advice componenet for large screens{

--- client/src/components/AdviceComponent.jsx
+++ client/src/components/AdviceComponent.jsx
@@ -8,40 +8,42 @@ function AdviceComponent (){
 return(
     <>
     <article className='advice-section'>
+        <div id='advice-wrapper'>
 
-        <section>
-            <i>
-                <img src={iconEating} alt="icon of ramen bowl" />
-            </i>
             <section>
-                <h3>Healthy eating</h3>
-                <p>
-                    Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity, and mood.
-                </p>
+                <i>
+                    <img src={iconEating} alt="icon of ramen bowl" />
+                </i>
+                <section>
+                    <h3>Healthy eating</h3>
+                    <p>
+                        Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity, and mood.
+                    </p>
+                </section>
             </section>
-        </section>
-        <section>
-            <i>
-                <img src={iconExercise} alt="icon of ramen bowl" />
-            </i>
             <section>
-                <h3>Regular exercise</h3>
-                <p>
-                Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity.
-                </p>
+                <i>
+                    <img src={iconExercise} alt="icon of ramen bowl" />
+                </i>
+                <section>
+                    <h3>Regular exercise</h3>
+                    <p>
+                    Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity.
+                    </p>
+                </section>
             </section>
-        </section>
-        <section>
-            <i>
-                <img src={iconSleep} alt="icon of ramen bowl" />
-            </i>
             <section>
-                <h3>Adequate sleep</h3>
-                <p>
-                Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation.
-                </p>
+                <i>
+                    <img src={iconSleep} alt="icon of ramen bowl" />
+                </i>
+                <section>
+                    <h3>Adequate sleep</h3>
+                    <p>
+                    Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation.
+                    </p>
+                </section>
             </section>
-        </section>
+        </div>
     </article>
     </>
 )
